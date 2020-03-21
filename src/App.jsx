7755eab7daa1f1cc52a915487a1b1b5eb641f744
azar <asarudeen26121997@gmail.com>
2020-03-22 00:29:19 +0530

    Imdb picks section imported

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import "./Header";
 import Header from "./Header";
 import Movies from "./Movies";
 import Slider from "./Slider";
+import Picks from "./Picks";
 
 const App = () => (
   <div className="IMDB-SITE">
     <Header />
     <Movies />
     <Slider />
+    <Picks />
   </div>
 );
 export default App;
