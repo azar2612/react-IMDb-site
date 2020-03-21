e62a33c4ca9d9cfc0b577b5bd5972fa3a993b65c
azar <asarudeen26121997@gmail.com>
2020-03-21 18:18:24 +0530

    Header section completedwith fonts addde

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import "./App.css";
+import "./Header";
+import Header from "./Header";
 
-const App = () => <div className="IMDB-SITE"></div>;
+const App = () => (
+  <div className="IMDB-SITE">
+    <Header />
+  </div>
+);
 export default App;
