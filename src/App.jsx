21e21117972f9a05912ae5a68f292a1e9d74bc36
azar <asarudeen26121997@gmail.com>
2020-03-22 00:11:12 +0530

    Imdb slider section imported

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import "./Header";
 import Header from "./Header";
 import Movies from "./Movies";
+import Slider from "./Slider";
 
 const App = () => (
   <div className="IMDB-SITE">
     <Header />
     <Movies />
+    <Slider />
   </div>
 );
 export default App;
