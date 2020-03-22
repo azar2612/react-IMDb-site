e04942f2cb804b0a233783d04195eaf8b0040aea
azar <asarudeen26121997@gmail.com>
2020-03-22 15:41:40 +0530

    IMDB rating section imported

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Movies from "./Movies";
 import Slider from "./Slider";
 import Picks from "./Picks";
 import Footer from "./Footer";
+import Rating from "./Rating";
 
 const App = () => (
   <div className="IMDB-SITE">
     <Header />
+    <Rating />
     <Movies />
     <Slider />
     <Picks />
