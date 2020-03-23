be3ed69815f4c9232ef57e4a110b57de503fe1f2
azar <asarudeen26121997@gmail.com>
2020-03-23 11:53:36 +0530

    IMDB picks section images and fonts added

--- src/Picks.jsx
+++ src/Picks.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import "./Picks.css";
+import Img1 from "./Picks_Images/21.jpg";
+import Img2 from "./Picks_Images/22.jpg";
+import Img3 from "./Picks_Images/23.jpg";
+import Img4 from "./Picks_Images/24.jpg";
+import Img5 from "./Picks_Images/25.jpg";
+import Img6 from "./Picks_Images/G.jpg";
 
 const Picks = () => (
   <div className="Picks">
@@ -24,27 +30,33 @@ const PicksHeader = () => (
 const PicksContainer = () => (
   <div className="Picks-container">
     <div className="Picks1">
-      <img src="" alt="k" />
+      <img src={Img1} alt="k" width="141" height="250" />
     </div>
     <div className="Picks2">
-      <img src="" alt="k" />
+      <img src={Img2} alt="k" width="141" height="250" />
     </div>
     <div className="Picks3">
-      <img src="" alt="k" />
+      <img src={Img3} alt="k" width="141" height="250" />
     </div>
     <div className="Picks4">
-      <img src="" alt="k" />
+      <img src={Img4} alt="k" width="141" height="250" />
     </div>
     <div className="Picks5">
-      <img src="" alt="k" />
+      <img src={Img5} alt="k" width="141" height="250" />
     </div>
     <div className="Picks6">
-      <img src="" alt="k" />
+      <img src={Img6} alt="k" width="145" height="250" />
     </div>
   </div>
 );
 const PicksFooter = () => (
   <div className="Picks-footer">
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque unde
+      autem amet aperiam, accusantium tempore impedit, labore fugit dolorem
+      facere provident quidem sapiente corrupti quis minima vel laudantium
+      incidunt architecto!
+    </p>
     <h2>VISIT THE IMDB PICKS SECTION</h2>
   </div>
 );
