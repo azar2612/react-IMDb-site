f4ab99ede5fa93a6338cec8a17eca233e9f5afd9
azar <asarudeen26121997@gmail.com>
2020-03-23 19:49:29 +0530

    IMDB rating section images and fonts added

--- src/Rating.jsx
+++ src/Rating.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Rating.css";
-
+import ImgA from "./Rating_Img/04.jpg";
 const Rating = () => (
   <div className="Rating">
     <RatingLeft />
@@ -11,7 +11,7 @@ const Rating = () => (
 
 const RatingLeft = () => (
   <div className="Rating-left">
-    <img src="" alt="r" />
+    <i class="fas fa-chevron-left"></i>
   </div>
 );
 
@@ -34,40 +34,112 @@ const RatingMiddle1 = () => (
 const RatingMiddle3 = () => (
   <div className="Rating-middle3">
     <h2>MAD MAX: FURY ROAD (2015)</h2>
-    <p>R 120MIN ACTION ADVENTURE SCIFI 15 MAY2016 (USA)</p>
+    <p>
+      R{" "}
+      <span>
+        {" "}
+        <i class="fas fa-circle"></i>
+      </span>
+      120 MIN{" "}
+      <span>
+        {" "}
+        <i class="fas fa-circle"></i>
+      </span>
+      ACTION ADVENTURE SCIFI 15 MAY2016 (USA)
+    </p>
   </div>
 );
 
 const RatingMiddle4 = () => (
   <div className="Rating-middle4">
-    <h2>MAD MAX: FURY ROAD (2015)</h2>
-    <p>R 120MIN ACTION ADVENTURE SCIFI 15 MAY2016 (USA)</p>
+    <div className="RateA">
+      <i class="fas fa-circle-notch"></i>
+      <p>8.7</p>
+    </div>
+    <div className="RateB">
+      <p>
+        Your rating:
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        6/10
+      </p>
+
+      <p>
+        Ratings: <span>8.7/</span>
+        <small>10</small> from <span> 137,182 users</span> Metascore:{" "}
+        <span>89/100</span>
+      </p>
+      <p>
+        Reviews:{" "}
+        <span>
+          697 user <i class="fas fa-circle"></i> 502 critic{" "}
+          <i class="fas fa-circle"></i> 46{" "}
+        </span>
+        from <span>Metacritic.com</span>
+      </p>
+    </div>
   </div>
 );
 
 const RatingMiddle5 = () => (
   <div className="Rating-middle5">
-    <h2>MAD MAX: FURY ROAD (2015)</h2>
-    <p>R 120MIN ACTION ADVENTURE SCIFI 15 MAY2016 (USA)</p>
+    <h2>SUMMARY</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate
+      laboriosam, Ducimus culpa ratione ullam quis ipsam!
+    </p>
+    <p>
+      Director: george miller{" "}
+      <span>
+        {" "}
+        <i class="fas fa-circle"></i>
+      </span>
+      Writers: George miller,Brendan McCarthy,Nick Lathouris{" "}
+      <span>
+        {" "}
+        <i class="fas fa-circle"></i>
+      </span>{" "}
+      Stars : <i class="far fa-user-circle"></i>{" "}
+      <i class="far fa-user-circle"></i> <i class="far fa-user-circle"></i>{" "}
+      <i class="far fa-user-circle"></i> <i class="far fa-user-circle"></i>{" "}
+      <i class="far fa-user-circle"></i>
+    </p>
   </div>
 );
 
 const RatingMiddle6 = () => (
   <div className="Rating-middle6">
-    <h2>MAD MAX: FURY ROAD (2015)</h2>
-    <p>R 120MIN ACTION ADVENTURE SCIFI 15 MAY2016 (USA)</p>
+    <button>
+      <i class="fas fa-plus"></i>
+      <p> WATCH LIST</p>
+      <i class="fas fa-sort-down"></i>
+    </button>
+    <button>
+      <i class="fas fa-play"></i>
+      <p>WATCH LATER</p>
+    </button>
   </div>
 );
 
 const RatingMiddle2 = () => (
   <div className="Rating-middle2">
-    <img src="" alt="m" />
+    <img src={ImgA} alt="k" width="170" height="400" />
   </div>
 );
 
 const RatingRight = () => (
   <div className="Rating-right">
-    <img src="" alt="o" />
+    <i class="fas fa-chevron-right"></i>
   </div>
 );
 
